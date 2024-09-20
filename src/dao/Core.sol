ca8bd1dfdf56ac8f5aaedcb012d61d9a8f5c3cb3
C2tP <c2tp@protonmail.com>
2024-09-20 13:49:19 +0900

    add skeleton code for Core and stable coin

---
+++ src/dao/Core.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+
+contract Core {
+
+    address public operator;
+    
+    event OperatorChanged(address indexed _address);
+    
+    constructor(address _operator){
+        operator = _operator;
+        emit OperatorChanged(_operator);
+    }
+
+    function getName() external pure returns (string memory) {
+        return "Core";
+    }
+
+    function setOperator(address _operator) external {
+        require(msg.sender == operator, "!operator");
+        
+        operator = _operator;
+        emit OperatorChanged(_operator);
+    }
+
+    function execute(
+        address _to,
+        uint256 _value,
+        bytes calldata _data
+    ) external returns (bool, bytes memory) {
+        require(msg.sender == operator,"!auth");
+
+        (bool success, bytes memory result) = _to.call{value:_value}(_data);
+
+        return (success, result);
+    }
+
+}
---
+++ src/protocol/StableCoin.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+
+contract StableCoin is ERC20 {
+
+    address public immutable owner;
+    mapping(address => bool) public operators;
+    event SetOperator(address indexed _op, bool _valid);
+
+    constructor(address _owner)
+        ERC20(
+            "StableCoin",
+            "usdXYZ"
+        )
+    {
+        owner = _owner;
+    }
+
+   function setOperator(address _operator, bool _valid) external {
+        require(msg.sender == owner, "!auth");
+        operators[_operator] = _valid;
+        emit SetOperator(_operator, _valid);
+    }
+
+    function mint(address _to, uint256 _amount) external {
+        require(operators[msg.sender], "!authorized");
+        
+        _mint(_to, _amount);
+    }
+
+    function burn(address _from, uint256 _amount) external {
+        require(operators[msg.sender], "!authorized");
+        
+        _burn(_from, _amount);
+    }
+}
