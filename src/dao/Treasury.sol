7bab0cb7a58db2a919321d06b3431a0bfab470c3
wavey0x <retro.waveyy@gmail.com>
2024-11-02 22:02:12 -0400

    feat: add Treasury.sol

---
+++ src/dao/Treasury.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.8.22;
+
+// These are the core Yearn libraries
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { Address } from "@openzeppelin/contracts/utils/Address.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { CoreOwnable } from "../dependencies/CoreOwnable.sol";
+
+contract Treasury is CoreOwnable {
+    using SafeERC20 for IERC20;
+    using Address for address;
+
+    event RetrieveToken (address token,uint amount);
+    event RetrieveETH (uint amount);
+    event FailedETHSend(bytes returnedData);
+
+    receive() external payable {}
+
+    constructor(address _core) CoreOwnable(_core) {}
+
+    //Retrieve full balance of token in contract
+    function retrieveToken(address _token, address _to) external onlyOwner {
+        retrieveTokenExact(_token, _to, IERC20(_token).balanceOf(address(this)));
+    }
+
+    function retrieveTokenExact(address _token, address _to, uint _amount) public onlyOwner {
+        IERC20(_token).safeTransfer(_to, _amount);
+        emit RetrieveToken(_token, _amount);
+    }
+
+    function retrieveETH(address _to) external onlyOwner {
+        retrieveETHExact(_to, address(this).balance);
+    }
+
+    function retrieveETHExact(address _to, uint _amount) public onlyOwner {
+        (bool success, bytes memory returnData) = _to.call{value: _amount}("");
+        if(!success) {emit FailedETHSend(returnData);}
+        require(success, "Sending ETH failed");
+        emit RetrieveETH(_amount);
+    }
+}
