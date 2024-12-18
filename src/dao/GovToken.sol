af86d9dc706c67da2635cf0eead11d1d91f9e17d
C2tP <c2tp@protonmail.com>
2024-12-18 10:05:11 +0900

    change token contracts to OFTs

--- src/dao/GovToken.sol
+++ src/dao/GovToken.sol
@@ -1,7 +1,10 @@
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import { CoreOwnable } from "../../src/dependencies/CoreOwnable.sol";
+// import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import { CoreOwnable } from "../../src/dependencies/CoreOwnable.sol";
 
-contract GovToken is ERC20, CoreOwnable {
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { OFT } from "@layerzerolabs/oft-evm/contracts/OFT.sol";
+
+contract GovToken is OFT {
     uint256 public immutable INITIAL_SUPPLY;
     bool public minterFinalized;
     address public minter;
@@ -20,9 +23,22 @@ contract GovToken is ERC20, CoreOwnable {
         uint256 _initialSupply,
         string memory _name,
         string memory _symbol
-    ) ERC20(_name, _symbol) CoreOwnable(_core) {
+    ) OFT(_name, _symbol, address(0x1a44076050125825900e736c501f859c50fE728c), _core)
+      Ownable(_core) {
         INITIAL_SUPPLY = _initialSupply;
         _mint(_vesting, _initialSupply);
+    }
+
+    function core() external returns(address){
+        return owner();
+    }
+
+    function _transferOwnership(address newOwner) internal override {
+        if(owner() == address(0)){
+            super._transferOwnership(newOwner);
+        }else{
+            revert OwnableInvalidOwner(newOwner);
+        }
     }
 
     function mint(address _to, uint256 _amount) external onlyMinter {
