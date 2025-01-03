90030a2ec97b01dfe941ea8983c8d387696e8b4e
wavey0x <retro.waveyy@gmail.com>
2025-01-03 07:21:33 -0500

    feat: add erc20 permit to both tokens

--- src/dao/GovToken.sol
+++ src/dao/GovToken.sol
@@ -1,7 +1,11 @@
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import { CoreOwnable } from "../../src/dependencies/CoreOwnable.sol";
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
-contract GovToken is ERC20, CoreOwnable {
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import { CoreOwnable } from "src/dependencies/CoreOwnable.sol";
+
+contract GovToken is ERC20Permit, CoreOwnable {
     uint256 public immutable INITIAL_SUPPLY;
     uint256 public globalSupply;
     bool public minterFinalized;
@@ -21,7 +25,10 @@ contract GovToken is ERC20, CoreOwnable {
         uint256 _initialSupply,
         string memory _name,
         string memory _symbol
-    ) ERC20(_name, _symbol) CoreOwnable(_core) {
+    ) 
+    ERC20(_name, _symbol)
+    ERC20Permit(_name)
+    CoreOwnable(_core) {
         INITIAL_SUPPLY = _initialSupply;
         _mint(_vesting, _initialSupply);
         globalSupply += _initialSupply;
