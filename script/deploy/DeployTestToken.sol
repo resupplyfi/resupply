be90ae21cfc2be934bbefbc2d313279f75d83197
C2tP <c2tp@protonmail.com>
2025-02-05 10:41:17 +0900

    update test deploy with createx

--- script/deploy/DeployTestToken.sol
+++ script/deploy/DeployTestToken.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.19;
 import { TenderlyHelper } from "../utils/TenderlyHelper.s.sol";
 import { console } from "lib/forge-std/src/console.sol";
 import "src/Constants.sol" as Constants;
-import { DeployScriptReturn } from "./DeployScriptReturn.sol";
+// import { DeployScriptReturn } from "./DeployScriptReturn.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import { ResupplyRegistry } from "src/protocol/ResupplyRegistry.sol";
@@ -25,53 +25,58 @@ import { RewardHandler } from "src/protocol/RewardHandler.sol";
 import { TestOFT } from "src/test/TestOFT.sol";
 import { ILayerZeroEndpointV2 } from "@layerzerolabs/lz-evm-protocol-v2/contracts/interfaces/ILayerZeroEndpointV2.sol";
 
+import { CreateXDeployer } from "script/utils/CreateXDeployer.s.sol";
 
 
-contract DeployTestToken is TenderlyHelper {
-    uint256 internal constant DEFAULT_MAX_LTV = 95_000; // 95% with 1e5 precision
-    uint256 internal constant DEFAULT_LIQ_FEE = 5_000; // 5% with 1e5 precision
-    uint256 internal constant DEFAULT_BORROW_LIMIT = 5_000_000 * 1e18;
-    uint256 internal constant DEFAULT_MINT_FEE = 0; //1e5 prevision
-    uint256 internal constant DEFAULT_PROTOCOL_REDEMPTION_FEE = 1e18 / 2; //half
+contract DeployTestToken is TenderlyHelper, CreateXDeployer {
+// contract DeployTestToken is CreateXDeployer {
 
-    function run() external returns (DeployScriptReturn[] memory _return) {
+
+    function run() external{
         address deployer = vm.rememberKey(vm.envUint("PK"));
         console.log(">>> deploying from:", deployer);
         vm.startBroadcast(deployer);
-        setEthBalance(deployer, 10 ether);
-        _return = deployEnvironment(deployer);
-    }
-
-    function setReturnData(address _address, bytes memory _constructor, string memory _name) private returns(DeployScriptReturn memory _return){
-        _return.address_ = _address;
-        _return.constructorParams = _constructor;
-        _return.contractName = _name;
+        // setEthBalance(deployer, 10 ether);
+        deployEnvironment(deployer);
     }
 
     function addressToBytes32(address _addr) public pure returns (bytes32) {
         return bytes32(uint256(uint160(_addr)));
     }
 
-    function deployEnvironment(address deployer) private returns (DeployScriptReturn[] memory _return) {
-        _return = new DeployScriptReturn[](1);
+    function deployEnvironment(address deployer) private{
 
         address _core = deployer;
+        // ICreateXDeployer createXDeployer = ICreateXDeployer(0xba5Ed099633D3B313e4D5F7bdc1305d3c28ba5Ed);
+        // TestOFT _token = new TestOFT(_core);
 
-        TestOFT _token = new TestOFT(_core);
+        bytes memory constructorArgs = abi.encode(address(_core));
+        bytes memory bytecode = abi.encodePacked(vm.getCode("Voter.sol:Voter"), constructorArgs);
+        uint88 randomness = uint88(uint256(keccak256(abi.encode("TestToken1"))));
+        bytes32 _salt = bytes32(uint256(uint160(deployer) + randomness));
+        bytes32 computedSalt = keccak256(abi.encode(_salt));
+        address computedAddress = createXDeployer.computeCreate3Address(computedSalt);
+        if (address(computedAddress).code.length == 0) {
+            computedAddress = createXDeployer.deployCreate3(_salt, bytecode);
+            console.log(string(abi.encodePacked("deployed to:")), address(computedAddress));
+        } else {
+            console.log(string(abi.encodePacked("already deployed at:")), address(computedAddress));
+        }
+
+        TestOFT _token = TestOFT(computedAddress);
         console.log("owner/core: ", _token.owner());
         _token.setOperator(deployer,true);
 
         _token.faucet(1_000 * 1e18);
         
         ILayerZeroEndpointV2 endpoint = ILayerZeroEndpointV2(0x1a44076050125825900e736c501f859c50fE728c);
+        // _token.setPeer(1, addressToBytes32(address(_token)));
         _token.setPeer(252, addressToBytes32(address(_token)));
 
         console.log("======================================");
         console.log("    Contracts     ");
         console.log("======================================");
-        for(uint256 i=0; i < _return.length; i++){
-            console.log(_return[i].contractName,": ", _return[i].address_);
-        }
+        console.log("testToken: ", computedAddress);
         console.log("======================================");
         console.log("balance of token: ", _token.balanceOf(deployer));
     }
