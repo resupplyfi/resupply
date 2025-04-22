b6552795b57281f4cb30ca8be9ead7f8b4c94e7d
C2tP <c2tp@protonmail.com>
2025-04-22 09:06:09 +0900

    choose slightly more robust exchange rate over gas savings

--- src/protocol/pair/ResupplyPairCore.sol
+++ src/protocol/pair/ResupplyPairCore.sol
@@ -568,24 +568,19 @@ abstract contract ResupplyPairCore is CoreOwnable, ResupplyPairConstants, Reward
         // Pull from storage to save gas and set default return values
         ExchangeRateInfo memory _exchangeRateInfo = exchangeRateInfo;
 
-        // Short circuit if already updated this block
-        if (_exchangeRateInfo.lastTimestamp != block.timestamp) {
-            // Get the latest exchange rate from the oracle
-            _exchangeRate = IOracle(_exchangeRateInfo.oracle).getPrices(address(collateral));
-            //convert price of collateral as debt is priced in terms of collateral amount (inverse)
-            _exchangeRate = 1e36 / _exchangeRate;
-
+        // Get the latest exchange rate from the oracle
+        //convert price of collateral as debt is priced in terms of collateral amount (inverse)
+        _exchangeRate = 1e36 / IOracle(_exchangeRateInfo.oracle).getPrices(address(collateral));
+        
+        //skip storage writes if value doesnt change
+        if (_exchangeRate != _exchangeRateInfo.exchangeRate) {
 
             // Effects: Bookkeeping and write to storage
             _exchangeRateInfo.lastTimestamp = uint96(block.timestamp);
             _exchangeRateInfo.exchangeRate = _exchangeRate;
             exchangeRateInfo = _exchangeRateInfo;
             emit UpdateExchangeRate(_exchangeRate);
-        } else {
-            // Use default return values if already updated this block
-            _exchangeRate = _exchangeRateInfo.exchangeRate;
         }
-
     }
 
     // ============================================================================================
