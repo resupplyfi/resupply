988ff9806af07aaccaa56be8c8704b3adc8a2a27
wavey0x <retro.waveyy@gmail.com>
2025-07-17 19:12:15 -0400

    chore: add interface

---
+++ src/dao/operators/TreasuryManagerV2.sol
@@ -0,0 +1,209 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import { ICore } from "src/interfaces/ICore.sol";
+import { ITreasury } from "src/interfaces/ITreasury.sol";
+import { CoreOwnable } from "src/dependencies/CoreOwnable.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { ISimpleReceiver } from "src/interfaces/ISimpleReceiver.sol";
+import { IPrismaFeeReceiver } from "src/interfaces/prisma/IPrismaFeeReceiver.sol";
+
+contract TreasuryManager is CoreOwnable {
+    using SafeERC20 for IERC20;
+
+    address public constant core = 0xc07e000044F95655c11fda4cD37F70A94d7e0a7d;
+    address public constant treasury = 0x4444444455bF42de586A88426E5412971eA48324;
+    address public constant prismaFeeReceiver = 0xfdCE0267803C6a0D209D3721d2f01Fd618e9CBF8;
+    address public manager;
+    ISimpleReceiver public lpIncentivesReceiver;
+
+    struct Permissions {
+        bool retrieveToken;
+        bool retrieveTokenExact;
+        bool retrieveETH;
+        bool retrieveETHExact;
+        bool setTokenApproval;
+        bool execute;
+        bool safeExecute;
+        bool transferTokenFromPrismaFeeReceiver;
+        bool approveTokenFromPrismaFeeReceiver;
+    }
+
+    event ManagerSet(address indexed manager);
+
+    modifier onlyManager() {
+        require(msg.sender == manager, "!manager");
+        _;
+    }
+
+    function retrieveToken(address _token, address _to) external onlyManager {
+        core.execute(
+            treasury,
+            abi.encodeWithSelector(
+                ITreasury.retrieveToken.selector,
+                _token,
+                _to
+            )
+        );
+    }
+
+    function retrieveTokenExact(address _token, address _to, uint256 _amount) external onlyManager {
+        core.execute(
+            treasury,
+            abi.encodeWithSelector(
+                ITreasury.retrieveTokenExact.selector,
+                _token,
+                _to,
+                _amount
+            )
+        );
+    }
+
+    function retrieveETH(address _to) external onlyManager {
+        core.execute(
+            treasury,
+            abi.encodeWithSelector(
+                ITreasury.retrieveETH.selector,
+                _to
+            )
+        );
+    }
+
+    function retrieveETHExact(address _to, uint256 _amount) external onlyManager {
+        core.execute(
+            treasury,
+            abi.encodeWithSelector(
+                ITreasury.retrieveETHExact.selector,
+                _to,
+                _amount
+            )
+        );
+    }
+
+    function setTokenApproval(address _token, address _spender, uint256 _amount) external onlyManager {
+        core.execute(
+            treasury,
+            abi.encodeWithSelector(
+                ITreasury.setTokenApproval.selector,
+                _token,
+                _spender,
+                _amount
+            )
+        );
+    }
+
+    /**
+     * @notice Execute an arbitrary call to the treasury
+     * @param _target The target address to call
+     * @param _data The data to call the target with
+     * @return success Whether the call was successful
+     * @return result The result of the call as bytes
+     * @dev Use `safeExecute` instead of this function if you need to ensure the call succeeds
+     */
+    function execute(address _target, bytes calldata _data) external onlyManager returns (bool, bytes memory result) {
+        result = core.execute(
+            treasury,
+            abi.encodeWithSelector(
+                ITreasury.execute.selector,
+                _target,
+                _data
+            )
+        );
+        return abi.decode(result, (bool, bytes));
+    }
+
+    /**
+     * @notice Safe execute an arbitrary call to the treasury
+     * @param _target The target address to call
+     * @param _data The data to call the target with
+     * @return result The result of the call as bytes
+     * @dev `safeExecute` enforces that the call must result in a success
+     */
+    function safeExecute(address _target, bytes calldata _data) external onlyManager returns (bytes memory result) {
+        result = core.execute(
+            treasury,
+            abi.encodeWithSelector(
+                ITreasury.safeExecute.selector,
+                _target,
+                _data
+            )
+        );
+        return abi.decode(result, (bytes));
+    }
+
+    function transferTokenFromPrismaFeeReceiver(address token, address to, uint256 amount) external onlyManager {
+        core.execute(
+            prismaFeeReceiver,
+            abi.encodeWithSelector(
+                bytes4(keccak256("transferToken(address,address,uint256)")),
+                token,
+                to,
+                amount
+            )
+        );
+    }
+
+    function approveTokenFromPrismaFeeReceiver(address token, address spender, uint256 amount) external onlyManager {
+        core.execute(
+            prismaFeeReceiver,
+            abi.encodeWithSelector(
+                bytes4(keccak256("setTokenApproval(address,address,uint256)")),
+                token,
+                spender,
+                amount
+            )
+        );
+    }
+
+    function claimLpIncentives() external onlyManager returns (uint256) {
+        return lpIncentivesReceiver.claimEmissions(manager);
+    }
+
+    function claimLpIncentivesTo(address _to) external onlyManager returns (uint256) {
+        return lpIncentivesReceiver.claimEmissions(_to);
+    }
+
+    function recoverERC20(IERC20 token) external onlyManager {
+        token.safeTransfer(manager, token.balanceOf(address(this)));
+    }
+
+    /**
+     * @notice Sets the manager address that can execute treasury operations
+     * @param _manager The address to set as the new manager
+     */
+    function setManager(address _manager) external onlyOwner {
+        manager = _manager;
+        emit ManagerSet(_manager);
+    }
+    
+    function setLpIncentivesReceiver(address _lpIncentivesReceiver) external onlyManager {
+        lpIncentivesReceiver = ISimpleReceiver(_lpIncentivesReceiver);
+    }
+
+    /**
+        @notice Helper function to view the active permissions granted to this contract
+        @return p The permissions struct
+    */
+    function viewPermissions() external view returns (Permissions memory p) {
+        p.retrieveToken = _hasPerm(address(treasury), ITreasury.retrieveToken.selector);
+        p.retrieveTokenExact = _hasPerm(address(treasury), ITreasury.retrieveTokenExact.selector);
+        p.retrieveETH = _hasPerm(address(treasury), ITreasury.retrieveETH.selector);
+        p.retrieveETHExact = _hasPerm(address(treasury), ITreasury.retrieveETHExact.selector);
+        p.setTokenApproval = _hasPerm(address(treasury), ITreasury.setTokenApproval.selector);
+        p.execute = _hasPerm(address(treasury), ITreasury.execute.selector);
+        p.safeExecute = _hasPerm(address(treasury), ITreasury.safeExecute.selector);
+        p.transferTokenFromPrismaFeeReceiver = _hasPerm(address(prismaFeeReceiver), IPrismaFeeReceiver.transferToken.selector);
+        p.approveTokenFromPrismaFeeReceiver = _hasPerm(address(prismaFeeReceiver), IPrismaFeeReceiver.setTokenApproval.selector);
+    }
+
+    function _hasPerm(address target, bytes4 selector) internal view returns (bool) {
+        (bool authorized,) = core.operatorPermissions(address(this), target, selector);
+        if (!authorized) (authorized,) = core.operatorPermissions(address(this), address(0), selector);
+        return authorized;
+    }
+
+    function owner() public view returns (address) {
+        return address(core);
+    }
+}
