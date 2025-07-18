c55a3c4b7aeb5767e4ed1d6724a4f9297809cadc
wavey0x <retro.waveyy@gmail.com>
2025-07-18 09:37:38 -0400

    chore: write addresses

--- test/integration/Utilities.t.sol
+++ test/integration/Utilities.t.sol
@@ -5,6 +5,7 @@ import { console } from "lib/forge-std/src/console.sol";
 import { Setup } from "test/integration/Setup.sol";
 import { Utilities } from "src/protocol/Utilities.sol";
 import { IResupplyRegistry } from "src/interfaces/IResupplyRegistry.sol";
+import { IResupplyPair } from "src/interfaces/IResupplyPair.sol";
 
 contract UtilitiesTest is Setup {
     Utilities public utilities;
@@ -17,14 +18,16 @@ contract UtilitiesTest is Setup {
     function test_GetPairInterestRates() public {
         address[] memory pairs = registry.getAllPairAddresses();
         console.log("Number of pairs:", pairs.length);
-        
+        console.log("Block timestamp:", block.timestamp);
+        console.log("Block number:", block.number);
         for (uint256 i = 0; i < pairs.length; i++) {
             address pair = pairs[i];
             uint256 underlyingRate = utilities.getUnderlyingSupplyRate(pair);
             uint256 sfrxusdRate = utilities.sfrxusdRates();
             uint256 rate = utilities.getPairInterestRate(pair);
+            address collateral = IResupplyPair(pair).collateral();
             console.log("--------------------------------");
-            console.log("Pair", pair);
+            console.log("Pair", pair, IResupplyPair(pair).name());
             console.log("%18e", underlyingRate * 365 * 86400, "underlying apr");
             console.log("%18e", sfrxusdRate * 365 * 86400, "sfrxusd apr");
             console.log("%18e", rate * 365 * 86400, "pair APR");
