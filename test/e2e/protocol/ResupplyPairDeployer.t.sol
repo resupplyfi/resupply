ad57b2e9b91c46bf039058cbba600bf6044d4bb4
wavey0x <retro.waveyy@gmail.com>
2025-07-25 11:49:19 -0400

    chore: make deploy function visibility external

--- test/e2e/protocol/ResupplyPairDeployer.t.sol
+++ test/e2e/protocol/ResupplyPairDeployer.t.sol
@@ -84,19 +84,27 @@ contract ResupplyPairDeployerTest is Setup {
     }
 
     function test_predictPairAddress() public {
-        address pairAddress = _predictPairAddress(
+        address pairAddressViaCustomConfig = _predictPairAddressWithCustomConfig(
             0,
             Mainnet.CURVELEND_SFRXUSD_CRVUSD,
             Mainnet.CONVEX_BOOSTER,
             uint256(Mainnet.CURVELEND_SFRXUSD_CRVUSD_ID)
         );
-        console2.log("Predicted Pair Address: ", pairAddress);
+        address predictedAddressViaDefaultConfig = _predictPairAddressWithDefaultConfig(
+            0,
+            Mainnet.CURVELEND_SFRXUSD_CRVUSD,
+            Mainnet.CONVEX_BOOSTER,
+            uint256(Mainnet.CURVELEND_SFRXUSD_CRVUSD_ID)
+        );
+        assertEq(pairAddressViaCustomConfig, predictedAddressViaDefaultConfig);
+        console2.log("Predicted Pair Address: ", pairAddressViaCustomConfig);
         ResupplyPair pair = deployLendingPair(
             0,
             Mainnet.CURVELEND_SFRXUSD_CRVUSD,
             uint256(Mainnet.CURVELEND_SFRXUSD_CRVUSD_ID)
         );
-        assertEq(pairAddress, address(pair));
+        assertEq(pairAddressViaCustomConfig, address(pair));
+        assertEq(predictedAddressViaDefaultConfig, address(pair));
     }
 
     function test_DeployPermissions(address _deployer) public {
@@ -175,7 +183,7 @@ contract ResupplyPairDeployerTest is Setup {
         assertEq(deployTime, uint40(block.timestamp));
     }
 
-    function _predictPairAddress(uint256 _protocolId, address _collateral, address _staking, uint256 _stakingId) internal view returns(address){
+    function _predictPairAddressWithCustomConfig(uint256 _protocolId, address _collateral, address _staking, uint256 _stakingId) internal view returns(address){
         address _pairAddress = deployer.predictPairAddress(
             _protocolId,
             abi.encode(
@@ -188,6 +196,16 @@ contract ResupplyPairDeployerTest is Setup {
                 DeploymentConfig.DEFAULT_MINT_FEE,
                 DeploymentConfig.DEFAULT_PROTOCOL_REDEMPTION_FEE
             ),
+            _staking,
+            _stakingId
+        );
+        return _pairAddress;
+    }
+
+    function _predictPairAddressWithDefaultConfig(uint256 _protocolId, address _collateral, address _staking, uint256 _stakingId) internal view returns(address){
+        address _pairAddress = deployer.predictPairAddress(
+            _protocolId,
+            _collateral,
             _staking,
             _stakingId
         );
@@ -275,7 +293,7 @@ contract ResupplyPairDeployerTest is Setup {
     }
 
     function test_PredictPairAddressWithDefaults() public {
-        address predictedAddress = deployer.predictPairAddress(
+        address predictedAddress = _predictPairAddressWithDefaultConfig(
             0,
             Mainnet.CURVELEND_SFRXUSD_CRVUSD,
             Mainnet.CONVEX_BOOSTER,
