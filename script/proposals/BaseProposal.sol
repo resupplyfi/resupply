62d61ae6cf8245a9db1efd76decae74b34a22dec
wavey0x <retro.waveyy@gmail.com>
2025-08-12 22:10:33 -0400

    feat: add cancelProposal permission prop script

---
+++ script/proposals/BaseProposal.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import { Protocol } from "src/Constants.sol";
+import { BaseAction } from "script/actions/dependencies/BaseAction.sol";
+import { IResupplyRegistry } from "src/interfaces/IResupplyRegistry.sol";
+import { IVoter } from "src/interfaces/IVoter.sol";
+import { ICore } from "src/interfaces/ICore.sol";
+import { console } from "lib/forge-std/src/console.sol";
+
+interface IPermastakerOperator {
+    function safeExecute(address target, bytes calldata data) external;
+    function createNewProposal(IVoter.Action[] calldata actions, string calldata description) external;
+}
+
+contract BaseProposal is BaseAction {
+    IResupplyRegistry public constant registry = IResupplyRegistry(Protocol.REGISTRY);
+    ICore public constant _core = ICore(Protocol.CORE);
+    IVoter public voter;
+    address public constant deployer = 0x4444AAAACDBa5580282365e25b16309Bd770ce4a;
+    IPermastakerOperator public constant PERMA_STAKER_OPERATOR = IPermastakerOperator(0x3419b3FfF84b5FBF6Eec061bA3f9b72809c955Bf);
+
+    function proposeVote(IVoter.Action[] memory actions) public {
+        addToBatch(
+            address(PERMA_STAKER_OPERATOR),
+            abi.encodeWithSelector(
+                IPermastakerOperator.createNewProposal.selector,
+                actions,
+                "Configure Operator Permissions"
+            )
+        );
+    }
+}
