a45e2d5603d5c2c9dc0eef9a7454549c463bfe60
wavey0x <retro.waveyy@gmail.com>
2025-10-22 20:36:53 -0400

    chore: fix filepath

--- test/integration/curveProposals/CurveProposalMintTest.t.sol
+++ test/integration/curveProposals/CurveProposalMintTest.t.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.28;
 import { Mainnet } from "src/Constants.sol";
 import { console } from "forge-std/console.sol";
 import { BaseCurveProposalTest } from "test/integration/curveProposals/BaseCurveProposalTest.sol";
-import { CurveProposalMint } from "script/proposals/CurveProposalMint.s.sol";
+import { CurveProposalMint } from "script/proposals/curve/CurveProposalMint.s.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 import { CurveLendOperator } from "src/dao/CurveLendOperator.sol";
