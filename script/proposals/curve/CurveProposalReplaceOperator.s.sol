4c62cdbb8c09ac067d5ab87f36b3eac64bbeefed
c2tp <c2tp@protonmail.com>
2025-10-28 10:25:42 +0900

    tests for replacing operator proposal

---
+++ script/proposals/curve/CurveProposalReplaceOperator.s.sol
@@ -0,0 +1,99 @@
+pragma solidity 0.8.28;
+
+import { Protocol, Mainnet } from "src/Constants.sol";
+import { console } from "lib/forge-std/src/console.sol";
+import { BaseCurveProposal } from "script/proposals/curve/BaseCurveProposal.sol";
+import { ICrvusdController } from 'src/interfaces/ICrvusdController.sol';
+import { ICurveLendMinterFactory } from 'src/interfaces/ICurveLendMinterFactory.sol';
+import { CurveLendOperator } from "src/dao/CurveLendOperator.sol";
+import { CurveLendMinterFactory } from "src/dao/CurveLendMinterFactory.sol";
+
+contract CurveProposalReplaceOperator is BaseCurveProposal {
+
+    address public deployer = Mainnet.CONVEX_DEPLOYER;
+
+    address public lendfactory;
+    address public market;
+
+    function run() public {
+
+        lendfactory = Mainnet.CURVE_LENDING_FACTORY;
+        market = Mainnet.CURVELEND_SREUSD_CRVUSD;
+
+        vm.startBroadcast(deployer);
+        bytes memory actions = buildProposalScript();
+
+        string memory metadata = "Update lending operator implementation. Increase lending to sreusd market to 10m.";
+        console.log("meta: ", metadata);
+        proposeOwnershipVote(actions, metadata);
+    }
+
+    function setDeployAddresses(address _factory, address _market) public{
+        lendfactory = _factory;
+        market = _market;
+    }
+
+    function buildProposalScript() public override returns (bytes memory script) {
+        BaseCurveProposal.Action[] memory actions = new BaseCurveProposal.Action[](6);
+
+        address newimplementation = address(0);//todo fill
+        //update implementation
+        actions[0] = BaseCurveProposal.Action({
+            target: Mainnet.CURVE_LENDING_FACTORY,
+            data: abi.encodeWithSelector(
+                CurveLendMinterFactory.setImplementation.selector, 
+                newimplementation)
+        });
+
+        //mint more for new operator
+        actions[1] = BaseCurveProposal.Action({
+            target: Mainnet.CURVE_CRVUSD_CONTROLLER,
+            data: abi.encodeWithSelector(
+                ICrvusdController.set_debt_ceiling.selector, 
+                lendfactory,
+                10_000_000e18)
+        });
+
+        //create new operator and fund
+        actions[2] = BaseCurveProposal.Action({
+            target: lendfactory,
+            data: abi.encodeWithSelector(
+                ICurveLendMinterFactory.addMarketOperator.selector, 
+                market,
+                10_000_000e18)
+        });
+
+        address oldoperator = address(0x6119e210E00d4BE2Df1B240D82B1c3DECEdbBBf0);
+
+        //reduce old operator cap to 0 setMintLimit(uint256)
+        actions[3] = BaseCurveProposal.Action({
+            target: oldoperator,
+            data: abi.encodeWithSelector(
+                CurveLendOperator.setMintLimit.selector, 
+                0)
+        });
+
+        //reduce old operator active amount to 0 reduceAmount(uint256)
+        actions[4] = BaseCurveProposal.Action({
+            target: oldoperator,
+            data: abi.encodeWithSelector(
+                CurveLendOperator.reduceAmount.selector, 
+                0)
+        });
+
+        //burn the returned amount  ICrvusdController.set_debt_ceiling(0)
+        actions[5] = BaseCurveProposal.Action({
+            target: Mainnet.CURVE_CRVUSD_CONTROLLER,
+            data: abi.encodeWithSelector(
+                ICrvusdController.set_debt_ceiling.selector, 
+                lendfactory,
+                0)
+        });
+
+        console.log("Number of actions:", actions.length);
+        console.log("lend factory at: ", lendfactory);
+        console.log("lend market at: ", market);
+
+        return buildScript(Mainnet.CURVE_OWNERSHIP_AGENT, actions);
+    }
+}
---
+++ test/integration/curveProposals/CurveProposalUpdateOperator.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import { Protocol, Mainnet } from "src/Constants.sol";
+import { console } from "forge-std/console.sol";
+import { BaseCurveProposalTest } from "test/integration/curveProposals/BaseCurveProposalTest.sol";
+import { ICurveVoting } from "src/interfaces/curve/ICurveVoting.sol";
+import { CurveProposalReplaceOperator } from "script/proposals/curve/CurveProposalReplaceOperator.s.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import { CurveLendOperator } from "src/dao/CurveLendOperator.sol";
+import { CurveLendMinterFactory } from "src/dao/CurveLendMinterFactory.sol";
+import { ICrvusdController } from 'src/interfaces/ICrvusdController.sol';
+
+contract CurveProposalUpdateOperator is BaseCurveProposalTest {
+    CurveProposalReplaceOperator proposalScript;
+
+    IERC20 public crvusd = IERC20(Mainnet.CRVUSD_ERC20);
+
+    CurveLendMinterFactory public factory;
+    IERC20 public market;
+
+    function setUp() public override {
+        super.setUp();
+        proposalScript = new CurveProposalReplaceOperator();
+        market = IERC20(Mainnet.CURVELEND_SREUSD_CRVUSD);
+
+        factory = CurveLendMinterFactory(Mainnet.CURVE_LENDING_FACTORY);
+
+        bytes memory script = proposalScript.buildProposalScript();
+
+        string memory metadata = "Update lending operator implementation. Increase lending to sreusd market to 10m.";
+        console.log("meta: ", metadata);
+        uint256 proposalId = proposeOwnershipVote(script, metadata);
+        console.log("crv supply balance: ", crvusd.totalSupply() );
+        simulatePassingOwnershipVote(proposalId);
+        executeOwnershipProposal(proposalId);
+    }
+
+    function test_mintAndSupply() public {
+        console.log("factory address: ", address(factory));
+        console.log("factory crvusd balance: ", crvusd.balanceOf(address(factory)) );
+        console.log("crv supply balance: ", crvusd.totalSupply() );
+
+        CurveLendOperator oldoperator = CurveLendOperator(0x6119e210E00d4BE2Df1B240D82B1c3DECEdbBBf0);
+        address operator = factory.markets(address(market));
+        console.log("supplied shares on operator: ", market.balanceOf(operator));
+        console.log("supplied shares on oldoperator: ", market.balanceOf(address(oldoperator)));
+
+        console.log("withdraw profit...");
+        oldoperator.withdraw_profit();
+        console.log("supplied shares on oldoperator: ", market.balanceOf(address(oldoperator)));
+    }
+
+
+}
