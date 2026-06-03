6ccf02ec38c5848b4f02ffbd88a090624205e0c6
wavey0x <wavey0x@proton.me>
2026-06-02 23:13:08 -0400

    proposal(ir): update calculator deployment and governance coverage

--- script/proposals/UpdateIRCalc.s.sol
+++ script/proposals/UpdateIRCalc.s.sol
@@ -5,9 +5,10 @@ import { BaseAction } from "script/actions/dependencies/BaseAction.sol";
 import { IVoter } from "src/interfaces/IVoter.sol";
 import { IResupplyPair } from "src/interfaces/IResupplyPair.sol";
 import { BaseProposal } from "script/proposals/BaseProposal.sol";
+import { ResupplyPairDeployer } from "src/protocol/ResupplyPairDeployer.sol";
 
 contract UpdateIRCalc is BaseAction, BaseProposal {
-    address public constant NEW_RATE_CALCULATOR = 0x42F9c30Fa365508B6Ce1c62B0269d3678aCcdffA;
+    address public constant NEW_RATE_CALCULATOR = 0xD3d5C6fc52f3bc29C3aB017d57D9A94A036Ca90f;
 
     function run() public isBatch(deployer) {
         IVoter.Action[] memory data = buildProposalCalldata();
@@ -24,9 +25,25 @@ contract UpdateIRCalc is BaseAction, BaseProposal {
         require(rateCalculator != address(0), "rate calculator not set");
         require(rateCalculator.code.length > 0, "rate calculator not deployed");
 
-        actions = new IVoter.Action[](numPairs);
+        address pairDeployer = registry.getAddress("PAIR_DEPLOYER");
+        ResupplyPairDeployer.ConfigData memory config = ResupplyPairDeployer(pairDeployer).defaultConfigData();
+
+        actions = new IVoter.Action[](numPairs + 1);
+        actions[0] = IVoter.Action({
+            target: pairDeployer,
+            data: abi.encodeWithSelector(
+                ResupplyPairDeployer.setDefaultConfigData.selector,
+                config.oracle,
+                rateCalculator,
+                config.maxLTV,
+                config.initialBorrowLimit,
+                config.liquidationFee,
+                config.mintFee,
+                config.protocolRedemptionFee
+            )
+        });
         for (uint256 i = 0; i < pairs.length; i++) {
-            actions[i] = IVoter.Action({
+            actions[i + 1] = IVoter.Action({
                 target: pairs[i],
                 data: abi.encodeWithSelector(
                     IResupplyPair.setRateCalculator.selector,
